e0e992e9a67e9222eea1cfe58dde4c63b4195628
rbral <rbral@student.touro.edu>
2024-06-26 16:51:53 -0400

    added model and lib files, works

--- app/src/main/java/com/example/random_number_generator/activities/MainActivity.java
+++ app/src/main/java/com/example/random_number_generator/activities/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.random_number_generator;
+package com.example.random_number_generator.activities;
 
 import android.os.Bundle;
 
+import com.example.random_number_generator.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
